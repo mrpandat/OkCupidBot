12005106eec3cceafe0f23ce9433fc5cf5bc643b
Juan <mrpandat@hotmail.fr>
2020-04-11 12:27:10 +0200

    Add some types.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ const OKCupid = require('okcupidjs');
 const dotenv = require("dotenv");
 const fs = require('fs');
 
-var okc = new OKCupid()
+const okc = new OKCupid()
 
 if(fs.existsSync('../.env')) {
     dotenv.config({ path: '.env' });
@@ -10,33 +10,37 @@ if(fs.existsSync('../.env')) {
     dotenv.config({ path: '.env.test' });
 }
 
-console.log(process.env);
+const {okc_username, okc_password} = process.env;
 
-okc.login('okc_username', 'okc_password', function(err, res, body) {
-  var query = {
-    "i_want": "women",
-    "minimum_age": 20,
-    "maximum_age": 30,
-    "cats": ["has"], 
-    "personality_filters": {
-      "adventuresome" : "more",
-      "artsiness" : "less"
-    },
-    "drugs": ["never"]
-  }
-
-  okc.search(query, function(err, res, body) {
-    if (err) {
-      console.log("Failed to get search results.");
-    } 
-    else {
-      // use the body object to extract the search results
-      var results = body.data;
-
-      console.log('Users:\n')
-      results.forEach(function(user) {
-        console.log(user.username + '\n');
-      });
+okc.login(okc_username, okc_password, function(err, res, body) {
+  if (err) {
+    console.log("Failed to login.");
+  } else {
+    var query = {
+      "i_want": "women",
+      "minimum_age": 20,
+      "maximum_age": 30,
+      "cats": ["has"], 
+      "personality_filters": {
+        "adventuresome" : "more",
+        "artsiness" : "less"
+      },
+      "drugs": ["never"]
     }
-  });
+  
+    okc.search(query, function(err, res, body) {
+      if (err) {
+        console.log("Failed to get search results.");
+      } 
+      else {
+        // use the body object to extract the search results
+        var results = body.data;
+  
+        console.log('Users:\n')
+        results.forEach(function(user) {
+          console.log(user.username + '\n');
+        });
+      }
+    });
+  }
 }) 
