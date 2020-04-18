31492226db036a25465ccb14c538a5a7fae9ebe6
Juan <mrpandat@hotmail.fr>
2020-04-18 16:59:41 +0200

    Make login async.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 const OKCupid = require('okcupidjs');
 const dotenv = require("dotenv");
 const fs = require('fs');
+const Promise = require('bluebird');
 
-const okc = new OKCupid()
+const okc = Promise.promisifyAll(new OKCupid());
 
 if(fs.existsSync('../.env')) {
     dotenv.config({ path: '.env' });
@@ -12,35 +13,37 @@ if(fs.existsSync('../.env')) {
 
 const {okc_username, okc_password} = process.env;
 
-okc.login(okc_username, okc_password, function(err, res, body) {
+okc.loginAsync(okc_username, okc_password, function(err, res, body) {
   if (err) {
     console.log("Failed to login.");
   } else {
-    var query = {
-      "i_want": "women",
-      "minimum_age": 20,
-      "maximum_age": 30,
-      "cats": ["has"], 
-      "personality_filters": {
-        "adventuresome" : "more",
-        "artsiness" : "less"
-      },
-      "drugs": ["never"]
-    }
-  
-    okc.search(query, function(err, res, body) {
-      if (err) {
-        console.log("Failed to get search results.");
-      } 
-      else {
-        // use the body object to extract the search results
-        var results = body.data;
-  
-        console.log('Users:\n')
-        results.forEach(function(user) {
-          console.log(user.username + '\n');
-        });
-      }
-    });
+    console.log("Login succed.")
   }
 }) 
+
+const query = {
+  "i_want": "women",
+  "minimum_age": 20,
+  "maximum_age": 30,
+  "cats": ["has"], 
+  "personality_filters": {
+    "adventuresome" : "more",
+    "artsiness" : "less"
+  },
+  "drugs": ["never"]
+};
+
+okc.search(query, function(err, res, body) {
+  if (err) {
+    console.log("Failed to get search results.");
+  } 
+  else {
+    // use the body object to extract the search results
+    var results = body.data;
+
+    console.log('Users:\n')
+    results.forEach(function(user) {
+      console.log(user.username + '\n');
+    });
+  }
+});
