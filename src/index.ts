eb5a330115ed2f564d7c0e69a299c08215682e45
Juan <mrpandat@hotmail.fr>
2020-04-21 00:08:53 +0200

    Fix login callback.

--- src/index.ts
+++ src/index.ts
@@ -17,34 +17,34 @@ okc.login(okc_username, okc_password, function(err, res, body) {
     console.log("Failed to login.");
     console.log(body);
     process.exit();
-  } else {
-    console.log("Login succed.")
   }
+  
+  console.log("Login succeed.")
+  
+  const query = {
+    "i_want": "women",
+    "minimum_age": 20,
+    "maximum_age": 30,
+    "cats": ["has"], 
+    "personality_filters": {
+      "adventuresome" : "more",
+      "artsiness" : "less"
+    },
+    "drugs": ["never"]
+  };
+  
+  okc.search(query, function(err, res, body) {
+    if (err) {
+      console.log("Failed to get search results.");
+    } 
+    else {
+      // use the body object to extract the search results
+      var results = body.data;
+  
+      console.log('Users:\n')
+      results.forEach(function(user) {
+        console.log(user.username + '\n');
+      });
+    }
+  });
 }) 
-
-const query = {
-  "i_want": "women",
-  "minimum_age": 20,
-  "maximum_age": 30,
-  "cats": ["has"], 
-  "personality_filters": {
-    "adventuresome" : "more",
-    "artsiness" : "less"
-  },
-  "drugs": ["never"]
-};
-
-okc.search(query, function(err, res, body) {
-  if (err) {
-    console.log("Failed to get search results.");
-  } 
-  else {
-    // use the body object to extract the search results
-    var results = body.data;
-
-    console.log('Users:\n')
-    results.forEach(function(user) {
-      console.log(user.username + '\n');
-    });
-  }
-});
